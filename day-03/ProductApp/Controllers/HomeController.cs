3a39a0239ea98d71ec045efee664bbfcd91f8aff
MMucahitNAS <MMucahitNAS@gmail.com>
2022-11-26 22:12:57 +0300

    Product add page has been added

---
+++ day-03/ProductApp/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using ProductApp.Models;
+
+namespace ProductApp.Controllers;
+
+public class HomeController : Controller
+{
+
+    public IActionResult Index()
+    {
+       
+        string msg = "Hello World!";
+        return View("Index",msg);
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+   
+
+    
+}
